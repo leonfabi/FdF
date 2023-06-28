8cabc95ce00165056779df49457996761427f1b1
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-06-28 14:50:50 +0200

    fixing norm errors

--- src/fdf_read_map.c
+++ src/fdf_read_map.c
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 08:35:56 by fkrug             #+#    #+#             */
-/*   Updated: 2023/06/28 12:55:41 by fkrug            ###   ########.fr       */
+/*   Updated: 2023/06/28 14:48:14 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,9 +53,9 @@ int	ft_read_map(t_mc *fdf, const char *pathname)
 		perror("Map empty");
 		return (-1);
 	}
+	fdf->x_len = ft_get_x_len(map);
 	while (map != NULL)
 	{
-		fdf->x_len = ft_get_x_len(map);
 		ft_lstadd_back(&(fdf->input), ft_lstnew(map));
 		map = get_next_line(fd);
 		fdf->y_len++;
@@ -63,6 +63,17 @@ int	ft_read_map(t_mc *fdf, const char *pathname)
 	close(fd);
 	free(map);
 	return (1);
+}
+
+void	ft_fill_data_array(t_mc *fdf, int x, int y, char **tmp)
+{
+	fdf->data[y][x].z = ft_atoi(tmp[x]);
+	fdf->data[y][x].y = y;
+	fdf->data[y][x].x = x;
+	if (fdf->data[y][x].z > fdf->max)
+		fdf->max = fdf->data[y][x].z;
+	if (fdf->data[y][x].z < fdf->min)
+		fdf->min = fdf->data[y][x].z;
 }
 
 int	ft_fill_data(t_mc *fdf)
@@ -81,13 +92,7 @@ int	ft_fill_data(t_mc *fdf)
 		x = 0;
 		while (tmp[x] != NULL && x < fdf->x_len)
 		{
-			fdf->data[y][x].z = ft_atoi(tmp[x]);
-			fdf->data[y][x].y = y;
-			fdf->data[y][x].x = x;
-			if (fdf->data[y][x].z > fdf->max)
-				fdf->max = fdf->data[y][x].z;
-			if (fdf->data[y][x].z < fdf->min)
-				fdf->min = fdf->data[y][x].z;
+			ft_fill_data_array(fdf, x, y, tmp);
 			x++;
 		}
 		ft_free_2d(tmp);
